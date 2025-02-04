2881343754084987c8b16d5a94f8962b4849782d
Devom Brahmbhatt <Devom.b@yahoo.com>
2025-02-03 21:00:06 -0800

    Remove Parallax Scroll View

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,12 @@
+import React from "react";
 import { Image, StyleSheet, Platform } from "react-native";
 
-import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 
 export default function HomeScreen() {
 	return (
-		<ParallaxScrollView
-			headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-		>
+		<>
 			<ThemedView style={styles.titleContainer}>
 				<ThemedText type="title">Welcome!</ThemedText>
 			</ThemedView>
@@ -53,7 +51,7 @@ export default function HomeScreen() {
 					<ThemedText type="defaultSemiBold">app-example</ThemedText>.
 				</ThemedText>
 			</ThemedView>
-		</ParallaxScrollView>
+		</>
 	);
 }
 
